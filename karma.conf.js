5d18cd6c5de489918a6fe653ebf2520df53b2e43
jerryleungwh <jerryleungwh@gmail.com>
2016-11-11 15:25:57 +0800

    increased coverage on some js, changed karma.conf.jf

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,8 @@ module.exports = function(config) {
 	  'lib/firebase.js',
 	  'lib/angularfire.min.js',	  
 	  'js/*.js',
-      'unit_tests/*.js'	  
+      'unit_tests/*.js',
+	  'https://www.gstatic.com/firebasejs/ui/live/0.5/firebase-ui-auth.js'
     ],
 	exclude: [
 	],
@@ -21,7 +22,10 @@ module.exports = function(config) {
 		 'js/index.js' : ['coverage'],
 		 'js/admin.js' : ['coverage'],
 		 'js/team.js' : ['coverage'],
-		 'js/member.js' : ['coverage']
+		 'js/member.js' : ['coverage'],
+		 'js/check_login.js' :['coverage'],
+		 'js/profile.js' :['coverage'],
+		 'js/verify.js' :['coverage']
 	},
 	reporters: ['progress', 'coverage'],
 	coverageReporter: {
@@ -32,7 +36,7 @@ module.exports = function(config) {
 	port: 8080,
 	colors: true,
     browsers: ['Chrome'],
-	singleRun: true,
+	singleRun: false,
     plugins: [
       'karma-chrome-launcher',      
       'karma-jasmine',
